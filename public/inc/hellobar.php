cf44b968e892b7cf90813b251606cfca20e17a5f
Johnie Hjelm <johnie.hjelm@symbio.com>
2015-02-16 22:18:21 +0100

    Minor fix

--- public/inc/hellobar.php
+++ public/inc/hellobar.php
@@ -1,5 +1,7 @@
+<?php body_class('hellobar__activate'); ?>
+
 <div class="hellobar hellobar--alert">
   <i class="icon ion-speakerphone hellobar-icon"></i>
-  <p>Just nu har vi störningar i sändningarna av Champions League.</p>
-  <button class="hellobar__close"><i class="icon ion-close"></i><span>Stäng</span></button>
+  <p><?php echo hellobar_get_plugin_option( 'title' ); ?></p>
+  <button class="hellobar__close"><i class="icon ion-close"></i><span><?php _e("Close notice", "hellobar"); ?></span></button>
 </div>
