fc5e082cf9f018120fae097f50f914bab62b0381
Johnie Hjelm <johnie.hjelm@symbio.com>
2015-02-16 19:58:40 +0100

    Now it doesn't save

--- hellobar.php
+++ hellobar.php
@@ -111,65 +111,6 @@ if ( ! class_exists( 'HelloBar' ) ) {
     }
 
     /**
-     * Get plugin options prefix.
-     *
-     * @return string
-     */
-    function _hellobar_get_plugin_options_prefix() {
-      return 'hellobar_plugin_option_';
-    }
-
-    /**
-     * Save plugin options data.
-     */
-    function _hellobar_save_plugin_options() {
-      $pattern = '/^' . str_replace( '_', '\_', _hellobar_get_plugin_options_prefix() ) . '.*/';
-      $data    = array();
-      $keys    = preg_grep( $pattern, array_keys( $_POST ) );
-
-      foreach ( $keys as $key ) {
-        if ( ! isset( $_POST[ $key ] ) ) {
-          continue;
-        }
-
-        // Fix for input fields that should be true or false.
-        if ( $_POST[ $key ] === 'on' ) {
-          $data[ $key ] = true;
-        } else {
-          $data[ $key ] = hellobar_remove_trailing_quotes( $_POST[ $key ] );
-        }
-      }
-
-      foreach ( $data as $key => $value ) {
-        add_option( $key, $value );
-      }
-    }
-
-    /**
-     * Get plugin option value.
-     *
-     * @param string $key
-     * @param mixed $default
-     *
-     * @return mixed
-     */
-    function hellobar_get_plugin_option( $key, $default ='' ) {
-      $prefix = _hellobar_get_plugin_options_prefix();
-      return get_option( $prefix . $key, $default );
-    }
-
-    /**
-     * Update plugin option.
-     *
-     * @param string $key
-     * @param mixed $value
-     */
-    function hellobar_update_plugin_option( $key, $value ) {
-      $prefix = _hellobar_get_plugin_options_prefix();
-      update_option( $prefix . $key, $value );
-    }
-
-    /**
      * Plugin menu page
      */
     function _hello_menu_page() {
@@ -224,6 +165,65 @@ function hellobar_is_method( $method ) {
 */
 function hellobar_remove_trailing_quotes( $str ) {
   return str_replace( "\'", "'", str_replace( '\"', '"', $str ) );
+}
+
+/**
+* Get plugin options prefix.
+*
+* @return string
+*/
+function _hellobar_get_plugin_options_prefix() {
+  return 'hellobar_plugin_option_';
+}
+
+/**
+* Save plugin options data.
+*/
+function _hellobar_save_plugin_options() {
+  $pattern = '/^' . str_replace( '_', '\_', _hellobar_get_plugin_options_prefix() ) . '.*/';
+  $data    = array();
+  $keys    = preg_grep( $pattern, array_keys( $_POST ) );
+
+  foreach ( $keys as $key ) {
+    if ( ! isset( $_POST[ $key ] ) ) {
+      continue;
+    }
+
+    // Fix for input fields that should be true or false.
+    if ( $_POST[ $key ] === 'on' ) {
+      $data[ $key ] = true;
+    } else {
+      $data[ $key ] = hellobar_remove_trailing_quotes( $_POST[ $key ] );
+    }
+  }
+
+  foreach ( $data as $key => $value ) {
+    add_option( $key, $value );
+  }
+}
+
+/**
+* Get plugin option value.
+*
+* @param string $key
+* @param mixed $default
+*
+* @return mixed
+*/
+function hellobar_get_plugin_option( $key, $default ='' ) {
+  $prefix = _hellobar_get_plugin_options_prefix();
+  return get_option( $prefix . $key, $default );
+}
+
+/**
+* Update plugin option.
+*
+* @param string $key
+* @param mixed $value
+*/
+function hellobar_update_plugin_option( $key, $value ) {
+  $prefix = _hellobar_get_plugin_options_prefix();
+  update_option( $prefix . $key, $value );
 }
 
 if ( !function_exists('hellobar')) {
