3a86316644de2744b15c84b6b8dbf40ff5bbc4dd
Johnie Hjelm <johnie.hjelm@symbio.com>
2015-02-18 09:42:43 +0100

    Fixed

--- public/inc/hellobar.php
+++ public/inc/hellobar.php
@@ -1,7 +1,11 @@
-<?php body_class('hellobar__activate'); ?>
-
 <div class="hellobar hellobar--alert">
   <i class="icon ion-speakerphone hellobar-icon"></i>
-  <p><?php echo hellobar_get_plugin_option( 'title' ); ?></p>
+  <p class="hellobar__title"><?php echo hellobar_get_plugin_option( 'title' ); ?></p>
+  <p class="hellobar__description">
+    <?php echo hellobar_get_plugin_option( 'description' ) . ' …'; ?>
+    <?php if ( hellobar_get_plugin_option( 'link' ) ): ?>
+      <a class="hellobar__link" href="<?php echo hellobar_get_plugin_option( 'link' ); ?>"><?php _e("Read more", "hellobar"); ?></a>
+    <?php endif; ?>
+  </p>
   <button class="hellobar__close"><i class="icon ion-close"></i><span><?php _e("Close notice", "hellobar"); ?></span></button>
 </div>
