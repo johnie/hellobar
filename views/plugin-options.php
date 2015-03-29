00af6cc88fa199dcfb52e2f63a69e4803778657f
Johnie Hjelm <johnie.hjelm@symbio.com>
2015-03-29 13:56:33 +0200

    Added mailchimp options

--- views/plugin-options.php
+++ views/plugin-options.php
@@ -21,6 +21,29 @@
         </tr>
       </tbody>
     </table>
+    <h3>Mailchimp setup</h3>
+    <table class="form-table">
+      <tbody>
+        <tr>
+          <th scope="row"><label for="hellobar_plugin_option_mailchimp_url"><?php _e('API URL', 'viapress'); ?></label></th>
+          <td>
+            <input type="text" name="hellobar_plugin_option_mailchimp_url" id="hellobar_plugin_option_mailchimp_url" value="<?php echo hellobar_get_plugin_option( 'mailchimp_url', '' ); ?>" placeholder="http://viaplay.us10.list-manage2.com/subscribe/">
+          </td>
+        </tr>
+        <tr>
+          <th scope="row"><label for="hellobar_plugin_option_mailchimp_token"><?php _e('API Token', 'viapress'); ?></label></th>
+          <td>
+            <input type="text" name="hellobar_plugin_option_mailchimp_token" id="hellobar_plugin_option_mailchimp_token" value="<?php echo hellobar_get_plugin_option( 'mailchimp_token', '' ); ?>" placeholder="412ec319f27466c765c25de0d">
+          </td>
+        </tr>
+        <tr>
+          <th scope="row"><label for="hellobar_plugin_option_mailchimp_listid"><?php _e('List ID', 'viapress'); ?></label></th>
+          <td>
+            <input type="text" name="hellobar_plugin_option_mailchimp_listid" id="hellobar_plugin_option_mailchimp_listid" value="<?php echo hellobar_get_plugin_option( 'mailchimp_listid', '' ); ?>" placeholder="0d27b3e3c3">
+          </td>
+        </tr>
+      </tbody>
+    </table>
   <?php submit_button(); ?>
   </form>
 </div>
