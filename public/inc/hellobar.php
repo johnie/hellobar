2823c00e92d2e849728070cc0e21e0f678fcc4da
Johnie Hjelm <johnie.hjelm@symbio.com>
2015-03-31 21:23:06 +0200

    minor fix

--- public/inc/hellobar.php
+++ public/inc/hellobar.php
@@ -1,43 +1,5 @@
 <?php
-  $activate_hellobar = hellobar_get_plugin_option( 'activation' );
-
-  $cat = null;
-  if (is_category()) {
-    $cat = get_queried_object();
-  }
-
-  $hellobar_args = array(
-    'post_type' => 'hellobar',
-    'post_status' => 'publish',
-    'posts_per_page' => 1
-  );
-
-  if ($cat != null) {
-     $hellobar_args['category__in'] = $cat->term_id;
-  }
-  else {
-    $hellobar_args['category__not_in'] = get_terms('category', array('fields' => 'ids')); //couldnt find a better way ... must be one but tried empty arr, null, empty string
-  }
-
-  $hellobars = new WP_Query($hellobar_args);
-
-  while ($hellobars->have_posts()) : $hellobars->the_post();
-
+  require_once(__DIR__."/../../../../services/controllers/hellobars.php");
+  $hellobarController = new hellobars();
+  echo $hellobarController->getHtml();
 ?>
-
-
-  <div class="hellobar hellobar--<?php echo get_post_meta( get_the_ID(), '_hellobar_type_select', true ); ?> <?php echo $activate_hellobar == 'on' ? 'hellobar__active' : ''; ?>" data-hellobar-id="<?php echo get_the_ID(); ?>">
-    <i class="icon ion-speakerphone hellobar-icon"></i>
-    <p class="hellobar__description">
-      <strong class="hellobar__title"><?php the_title(); ?></strong>
-      <?php echo wp_trim_words(get_the_excerpt(), 15); ?>
-      <?php if ( get_post_meta( get_the_ID(), '_hellobar_type_link', true ) != "" ): ?>
-         <a class="hellobar__link" href="<?php echo get_post_meta( get_the_ID(), '_hellobar_type_link', true ); ?>"><?php _e("Read more", "hellobar"); ?></a>
-      <?php else: ?>
-        <a class="hellobar__link" href="<?php the_permalink(); ?>"><?php echo(get_post_meta( get_the_ID(), '_hellobar_button_text', true ) ?: _e('Read more', 'viapress')); ?></a>
-      <?php endif; ?>
-    </p>
-    <button class="hellobar__close"><i class="icon ion-close"></i><span><?php _e("Close notice", "hellobar"); ?></span></button>
-  </div>
-
-<?php endwhile; ?>
